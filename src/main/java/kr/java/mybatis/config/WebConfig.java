2f97e2d040306019430cfd57d5595844dcda88af
kimjava911 <aibackend4th@gmail.com>
2025-11-20 10:11:05 +0900

    step1

---
+++ src/main/java/kr/java/mybatis/config/WebConfig.java
@@ -0,0 +1,17 @@
+package kr.java.mybatis.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan("kr.java.mybatis") // 패키지의 실질적인 최상단
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>게시판</title>
+</head>
+<body>
+    <h1>게시판에 오신 걸 환영합니다</h1>
+    <p>무엇을 적으시겠어요?</p>
+</body>
+</html>
