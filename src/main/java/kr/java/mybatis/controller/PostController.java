e0de5d145d88ef3185c4038ea179afaaeb23b7ef
kimjava911 <aibackend4th@gmail.com>
2025-11-20 14:43:45 +0900

    step5 service

--- src/main/java/kr/java/mybatis/controller/PostController.java
+++ src/main/java/kr/java/mybatis/controller/PostController.java
@@ -1,5 +1,6 @@
 package kr.java.mybatis.controller;
 
+import kr.java.mybatis.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/post")
 public class PostController {
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping
     public String post() {
         return "post";
