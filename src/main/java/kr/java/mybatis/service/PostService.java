8ede0639af03d305736a5b35971a87e50982763f
kimjava911 <aibackend4th@gmail.com>
2025-11-20 14:31:46 +0900

    step5 dto

--- src/main/java/kr/java/mybatis/service/PostService.java
+++ src/main/java/kr/java/mybatis/service/PostService.java
@@ -1,5 +1,11 @@
 package kr.java.mybatis.service;
 
+import kr.java.mybatis.model.dto.PostDTO;
+import kr.java.mybatis.model.dto.PostUpdateDTO;
+import kr.java.mybatis.model.dto.PostWriteDTO;
+
+import java.util.List;
+
 public interface PostService {
     void write(PostWriteDTO dto);
     PostDTO findById();
